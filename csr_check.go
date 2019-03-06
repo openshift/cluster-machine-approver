374b20416ec80f8a05a5b178d6b2313553c04a0f
Colin Walters <walters@verbum.org>
2019-03-06 22:46:30 +0000

    Add CSR failure cause to logs
    My worker's CSRs aren't being approved and this would help me
    debug why.

--- csr_check.go
+++ csr_check.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/x509"
+	"fmt"
 	"strings"
 
 	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
@@ -17,25 +18,25 @@ const (
 	nodeUserPrefix = nodeUser + ":"
 )
 
-func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr *x509.CertificateRequest) (string, bool) {
+func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr *x509.CertificateRequest) (string, error) {
 	if !strings.HasPrefix(req.Spec.Username, nodeUserPrefix) {
-		return "",false
+		return "",fmt.Errorf("Doesn't match expected prefix")
 	}
 
 	nodeAsking := strings.TrimPrefix(req.Spec.Username, nodeUserPrefix)
 	if len(nodeAsking) < 1 {
-		return "",false
+		return "",fmt.Errorf("Empty name")
 	}
 
 	// Check groups, we need at least:
 	// - system:nodes
 	// - system:authenticated
 	if len(req.Spec.Groups) < 2 {
-		return "",false
+		return "",fmt.Errorf("Too few groups")
 	}
 	groupSet := sets.NewString(req.Spec.Groups...)
 	if !groupSet.HasAll(nodeGroup, "system:authenticated") {
-		return "",false
+		return "",fmt.Errorf("Not in system:authenticated")
 	}
 
 	// Check usages, we need only:
@@ -43,7 +44,7 @@ func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr
 	// - key encipherment
 	// - server auth
 	if len(req.Spec.Usages) != 3 {
-		return "",false
+		return "",fmt.Errorf("Too few usages")
 	}
 
 	usages := make([]string, 0)
@@ -53,7 +54,7 @@ func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr
 
 	// No extra usages!
 	if len(usages) != 3 {
-		return "",false
+		return "",fmt.Errorf("Unexpected usages: %d", len(usages))
 	}
 
 	usageSet := sets.NewString(usages...)
@@ -62,12 +63,12 @@ func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr
 		string(certificatesv1beta1.UsageKeyEncipherment),
 		string(certificatesv1beta1.UsageServerAuth),
 	) {
-		return "",false
+		return "",fmt.Errorf("Missing usages")
 	}
 
 	// Check subject: O = system:nodes, CN = system:node:ip-10-0-152-205.ec2.internal
 	if csr.Subject.CommonName != req.Spec.Username {
-		return "",false
+		return "",fmt.Errorf("Mismatched CommonName %s != %s", csr.Subject.CommonName, req.Spec.Username)
 	}
 
 	var hasOrg bool
@@ -78,23 +79,23 @@ func validateCSRContents(req *certificatesv1beta1.CertificateSigningRequest, csr
 		}
 	}
 	if !hasOrg {
-		return "",false
+		return "",fmt.Errorf("Organization doesn't include %s", nodeGroup)
 	}
 
-	return nodeAsking, true
+	return nodeAsking, nil
 }
 
 // authorizeCSR authorizes the CertificateSigningRequest req for a node's server certificate.
 // csr should be the parsed CSR from req.Spec.Request. Names contained in the CSR are checked against addresses in the
 // corresponding node's machine status.
-func authorizeCSR(machineList *v1beta1.MachineList, req *certificatesv1beta1.CertificateSigningRequest, csr *x509.CertificateRequest) bool {
+func authorizeCSR(machineList *v1beta1.MachineList, req *certificatesv1beta1.CertificateSigningRequest, csr *x509.CertificateRequest) error {
 	if machineList == nil || len(machineList.Items) < 1 || req == nil || csr == nil {
-		return false
+		return fmt.Errorf("Invalid request")
 	}
 
-	nodeAsking, ok := validateCSRContents(req, csr)
-	if !ok {
-		return false
+	nodeAsking, err := validateCSRContents(req, csr)
+	if err != nil {
+		return err
 	}
 	// Check that we have a registered node with the request name
 	var targetMachine *v1beta1.MachineStatus
@@ -105,7 +106,7 @@ func authorizeCSR(machineList *v1beta1.MachineList, req *certificatesv1beta1.Cer
 		}
 	}
 	if targetMachine == nil {
-		return false
+		return fmt.Errorf("No target machine")
 	}
 
 	// SAN checks for both DNS and IPs, e.g.,
@@ -115,19 +116,23 @@ func authorizeCSR(machineList *v1beta1.MachineList, req *certificatesv1beta1.Cer
 		if len(san) < 1 {
 			continue
 		}
+		var attemptedAddresses []string
 		var foundSan bool
 		for _, addr := range targetMachine.Addresses {
 			switch addr.Type {
 			case v1.NodeInternalDNS, v1.NodeExternalDNS, v1.NodeHostName:
 				if san == addr.Address {
 					foundSan = true
+					break
+				} else {
+					attemptedAddresses = append(attemptedAddresses, addr.Address)
 				}
 			default:
 			}
 		}
 		// The CSR requested a DNS name that did not belong to the machine
 		if !foundSan {
-			return false
+			return fmt.Errorf("DNS name '%s' not in machine names: %s", san, strings.Join(attemptedAddresses, " "))
 		}
 	}
 
@@ -135,21 +140,25 @@ func authorizeCSR(machineList *v1beta1.MachineList, req *certificatesv1beta1.Cer
 		if len(san) < 1 {
 			continue
 		}
+		var attemptedAddresses []string
 		var foundSan bool
 		for _, addr := range targetMachine.Addresses {
 			switch addr.Type {
 			case v1.NodeInternalIP, v1.NodeExternalIP:
 				if san.String() == addr.Address {
 					foundSan = true
+					break
+				} else {
+					attemptedAddresses = append(attemptedAddresses, addr.Address)
 				}
 			default:
 			}
 		}
 		// The CSR requested an IP name that did not belong to the machine
 		if !foundSan {
-			return false
+			return fmt.Errorf("IP address '%s' not in machine addresses: %s", san, strings.Join(attemptedAddresses, " "))
 		}
 	}
 
-	return true
+	return nil
 }
