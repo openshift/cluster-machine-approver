a443fb1271a5372d2c5940f75cc15e90ca8fc85a
Joel Speed <joel.speed@hotmail.co.uk>
2021-12-03 11:26:08 +0000

    Configure mapi_old_pending_csr count metric

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -15,6 +15,8 @@ const DefaultMetricsPort = ":9191"
 var (
 	// CurrentPendingCSRCountDesc is a metric to report count of the pending csr in the cluster
 	CurrentPendingCSRCountDesc = prometheus.NewDesc("mapi_current_pending_csr", "Count of pending CSRs at the cluster level", nil, nil)
+	// OldPendingCSRCountDesc is a metric to report the count of pending CSRs which have aged out of the approval window
+	OldPendingCSRCountDesc = prometheus.NewDesc("mapi_old_pending_csr", "Count of pending CSRs which are now too old to be approved", nil, nil)
 	// MaxPendingCSRDesc is a metric to report threshold value of the pending csr beyond which csr will be ignored
 	MaxPendingCSRDesc = prometheus.NewDesc("mapi_max_pending_csr", "Threshold value of the pending CSRs beyond which any new CSR requests will be ignored ", nil, nil)
 )
@@ -34,12 +36,14 @@ func (mc *MetricsCollector) Collect(ch chan<- prometheus.Metric) {
 // Describe implements the prometheus.Collector interface.
 func (mc MetricsCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- CurrentPendingCSRCountDesc
+	ch <- OldPendingCSRCountDesc
 	ch <- MaxPendingCSRDesc
 }
 
 // Collect implements the prometheus.Collector interface.
 func (mc MetricsCollector) collectMetrics(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(CurrentPendingCSRCountDesc, prometheus.GaugeValue, float64(atomic.LoadUint32(&controller.PendingCSRs)))
+	ch <- prometheus.MustNewConstMetric(OldPendingCSRCountDesc, prometheus.GaugeValue, float64(atomic.LoadUint32(&controller.OldPendingCSRs)))
 	ch <- prometheus.MustNewConstMetric(MaxPendingCSRDesc, prometheus.GaugeValue, float64(atomic.LoadUint32(&controller.MaxPendingCSRs)))
 	klog.V(4).Infof("collectMetrics exit")
 }
